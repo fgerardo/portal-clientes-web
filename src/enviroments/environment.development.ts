ff4e611a36ad6da0c3ea8e6410c3716fea1567f3
BLUEIT\francisco.gonzalez <francisco.gonzalez@qaracter.com>
2025-04-15 00:21:39 -0600

    Se suben cambios de la spropiedades de enviroment

--- src/enviroments/environment.development.ts
+++ src/enviroments/environment.development.ts
@@ -4,6 +4,7 @@
 const URL_BASE =
   'http://localhost:8092/portal-clientes-services/';
 const URL_BASE_INTERMEDIARIO = 'http://localhost:8089/intermediario-service/'
+const URL_BASE_INCODE = 'https://portalb.allianz.com.mx/'
 
 export const environment = {
   production: false,
@@ -15,4 +16,25 @@ export const environment = {
     procesarImagen: URL_BASE_INTERMEDIARIO + 'portalClientes/v1/procesarImagen',
     verificarImagenUsuario: URL_BASE_INTERMEDIARIO + 'portalClientes/v1/verificarImagen'
   },
+  urlIncode: {
+    urlLigaIncode: URL_BASE_INCODE + 'liga-incode/notificaciones-incode?idSac=',
+    urlLigaVerificacionVdentidad: URL_BASE_INCODE + 'liga-incode/verificacion-identidad',
+    urlGetEnmascararData: URL_BASE_INCODE + 'incode-services/v1/get-enmascarar-data',
+    urlIsExpedienteCompletoRfc: URL_BASE_INCODE + 'expediente-identificacion-service/v1/expediente/estatus/',
+    urlServicioConsultaCG: URL_BASE_INCODE + 'condiciones-generales-services/v1/consulta/',
+    urlServicioRegistrarFirmasCG: URL_BASE_INCODE + 'incode-services/v1/docFirmas/condiciones/generales',
+    urlApiAvailableToken: URL_BASE_INCODE + 'incode-services/v1/availableToken',
+    urlApiNotificacionFace: URL_BASE_INCODE + 'incode-services/notificacion/v1/face-recognition/',
+    urlApiConsultProcess: URL_BASE_INCODE + 'incode-services/incode/consult/process/',
+    urlApiConsultIntentos: URL_BASE_INCODE + 'incode-services/incode/consult/intentos',
+    urlServicioNombreCliente: URL_BASE_INCODE + 'expediente-identificacion-service/v1/nombre/cliente/',
+    urlServicioValidarEndosoCobro: URL_BASE_INTERMEDIARIO + 'b2b-datoscliente-service/api/datos/cliente/validaEndosoDiaCobro',
+    urlServicioValidarRemesasIdepol: URL_BASE_INCODE + 'b2b-datoscliente-service/api/datos/cliente/validaRemesasIdepol',
+    urlServicioActualizarDiaCobro: URL_BASE_INCODE + 'b2b-datoscliente-service/api/datos/cliente/actualizarDiaCobro',
+    urlServicioRegistrarBitacoraContacto: URL_BASE_INCODE + 'condiciones-generales-services/v1/modificacion-cliente',
+    urlEnrolamientoIncode: URL_BASE + 'expediente-identificacion-service/incode/obtener/liga/biometrico/pc',
+    urlServicioSaveProcess: URL_BASE_INCODE + 'incode-services/incode/save/process',
+    consultEstatusServiciosIncode: URL_BASE_INCODE +'incode-services/v1/getEstatusIncode',
+    urlQuejasSugerencias: 'https://www.allianz.com.mx/servicios/tramites/quejas-sugerencias.html'
+  }
 };
